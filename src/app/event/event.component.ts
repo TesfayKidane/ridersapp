bb2e858f72310009147ede7ae7c5fafc147744f6
Tesfay Aregay <Tesfay Aregay>
2017-04-24 01:15:34 -0500

    aslkd

---
+++ src/app/event/event.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import {EventService} from '../services/event.service';
+import {EventModel} from '../models/EventModel';
+
+@Component({
+  selector: 'app-event',
+  templateUrl: './event.component.html',
+  styleUrls: ['./event.component.css']
+})
+export class EventComponent implements OnInit {
+  submitted  = false;
+  eventForm: FormGroup;
+  private eventModel = new EventModel('', '', '', 0, '', '', 0, new Date(), 1, '');
+  constructor(public fb: FormBuilder, public eventService: EventService) {
+    this.eventForm = fb.group({
+      'eventName' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventStartCity' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventStartState' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventStartPostCode' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventEndCity' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventEndState' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventEndPostCode' : ['', [Validators.required, Validators.minLength(1)]],
+      'eventStartDateTime' : ['', [Validators.required, Validators.minLength(1)]],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    // console.log(this.eventForm.value);
+    // this.eventService.getEvents().subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //   },
+    //   (err) => {
+    //     console.log(err);
+    //   }
+    // );
+    this.eventModel = this.eventForm.value;
+    this.eventModel.eventOwnerId = 1;
+    this.eventModel.eventStatus = 'started';
+    // console.log(this.eventModel);
+    this.eventService.postEvent(this.eventModel)
+      .subscribe(
+      events => { console.log(events); },
+      err => {console.log(err); }
+    );
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/models/EventModel.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Tesfay on 4/23/2017.
+ */
+
+export class EventModel {
+  constructor(
+  public eventName: string,
+  public eventStartCity: string,
+  public eventStartState: string,
+  public eventStartPostCode: number,
+  public eventEndCity: string,
+  public eventEndState: string,
+  public eventEndPostCode: number,
+  public eventStartDateTime: Date,
+  public eventOwnerId: number,
+  public eventStatus: string) {}
+}
