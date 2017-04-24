a4ff37266c191ac14c31d9891cd3f2dfab66a9fa
MiGa <jaraaxai@yahoo.com>
2017-04-24 13:22:00 -0500

    Routes are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,26 @@ import {EventService} from './services/event.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule } from '@angular/material';
 import 'hammerjs';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClubComponent } from './club/club.component';
+
+const appRoutes: Routes = [
+  { path: 'homepage', component: HomeComponent },
+  { path: 'event', component: EventComponent },
+  { path: 'club', component: ClubComponent },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent
+    EventComponent,
+    HomeComponent,
+    ClubComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
