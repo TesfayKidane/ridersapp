5903311f99d761f6677d1ff9f39d4cb74ea955c5
sherxon <sheraliobidov1199@gmail.com>
2017-04-25 20:23:07 -0500

    added chat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GoogleMapService} from "../services/googlemap.service";
+import {ClubService} from "../services/club.service";
 import {Auth} from '../services/auth.service';
 
 @Component({
@@ -7,10 +9,63 @@ import {Auth} from '../services/auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // google maps zoom level
+  zoom: number = 10;
 
-  constructor(private auth: Auth) { }
+  // initial center position for the map
+  lat: number = 41.018210;
+  lng: number = -91.970417;
+  location = {};
+  markers: Marker[] = [];
+  showCircle= false;
+  btnText = 'Select Clubs';
+  constructor(public mapdb: ClubService, private auth: Auth) { }
 
   ngOnInit() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
+    }
   }
-
+  showClubs() {
+    if(this.btnText === 'Select Clubs') {
+      this.btnText = 'Show Clubs';
+      this.showCircle = true;
+      this.mapdb.getNearbyClubs(this.lat, this.lng).subscribe(s => {
+        const json = s.json();
+        for (const key in json) {
+          this.markers.push({
+            lat: json[key].coords.lat,
+            lng: json[key].coords.lng,
+            draggable: true
+          });
+        }
+      });
+    }else {
+      this.btnText = 'Select Clubs';
+      this.showCircle = false;
+    }
+  }
+  setPosition(position) {
+    this.location = position.coords;
+    console.log(position.coords);
+  }
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
+  markerDragEnd(m: Marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+  mapClicked($event) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+}
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
 }
