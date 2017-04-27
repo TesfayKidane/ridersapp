50ea52e1061b73b0569e75293def3dd512a8d222
Tesfay Aregay <Tesfay Aregay>
2017-04-26 23:35:05 -0500

    form update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # enable production mode on your angular project
 # edit .gitignore and remove "/dist"
 # create repo on github and fetch the github repo link
-ng build --prod --base-href="https://tesfaykidane.github.io/angular-forms/"
+ng build --prod --base-href="https://tesfaykidane.github.io/ridersapp/"
 git add .
 git commit -m "form update"
 git push
