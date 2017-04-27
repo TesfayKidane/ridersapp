2dc4748c2e01089b5f949157f86888f6e99dfd7f
Tesfay Aregay <Tesfay Aregay>
2017-04-27 09:23:18 -0500

    authofix

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,8 +7,19 @@ import {SharedService} from './SharedService';
 export class UserService {
   constructor(public http: Http) {
   }
+
+  // var options = { method: 'POST',
+  // url: 'https://bikeriders.auth0.com/oauth/token',
+  // headers: { 'content-type': 'application/json' },
+  // postBody = '{"client_id":"zGMhC1ZDySXuF9by1TFRMT8cdixqFJtl","client_secret":"2hiseyWC1fBlWAauawyHM_cSH2sHn9f4w9hzg5r9X-6E5750Nu_wsrbPqufx7bDw","audience":"http://localhost:9000","grant_type":"client_credentials"}' ;
+  // postToGetAPIAccessToken(){
+  //   const url = 'https://bikeriders.auth0.com/oauth/token';
+  //   return this.http.post(url, this.postBody, SharedService.API_JSON_HEADER())
+  //     .map((res: Response) => {res.json())
+  //     .catch((err: any) => Observable.throw('Error Posting to Server'));
+  // }
   postUser(body: Object): Observable<Object>  {
-    return this.http.post(SharedService.API_USER_ADD, body, SharedService.API_REQUEST_OPTIONS())
+       return this.http.post(SharedService.API_USER_ADD, body, SharedService.API_REQUEST_OPTIONS())
       .map((res: Response) => res.json())
       .catch((err: any) => Observable.throw('Error Posting to Server'));
   }
