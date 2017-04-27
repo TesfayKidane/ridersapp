12ac1d6bd8b7f2d75f50be20440b79a5e2187ea1
Tesfay Aregay <Tesfay Aregay>
2017-04-26 23:33:01 -0500

    deployscriptadded

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# enable production mode on your angular project
+# edit .gitignore and remove "/dist"
+# create repo on github and fetch the github repo link
+ng build --prod --base-href="https://tesfaykidane.github.io/angular-forms/"
+git add .
+git commit -m "form update"
+git push
+git subtree push --prefix dist origin gh-pages
